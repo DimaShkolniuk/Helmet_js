2206d326204fe218eb8b77ac780d624e4c36bfa7
Dima SH <markrais.16@gmail.com>
2024-05-04 01:27:20 +0300

    add protect against Clickjacking, XSS, MIME sniffing, HTTP opening

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ const app = express()
 
 module.exports = app
 const api = require('./server.js')
+app.use(helmet.hidePoweredBy())
+app.use(helmet.frameguard({ action: 'deny' })) //protect against Clickjacking.
+app.use(helmet.xssFilter()) //protect against XSS.
+app.use(helmet.noSniff()) //protect against MIME sniffing.
+app.use(helmet.ieNoOpen()) //protect against HTTP opening on the Internet Explorer
 app.use(express.static('public'))
 app.disable('strict-transport-security')
 app.use('/_api', api)
